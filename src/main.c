846c45626b68a368fca8957b9be1c7d0bd515bc1
Scott Brackett <scottybrackett@gmail.com>
2015-06-29 16:01:28 -0500

    Add more comments to code

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 /*
  * main.c
- * Sets up a Window and BitmapLayer, loads all of the PNGs that make up the animation ad GBitmaps, then advances through the frames when the data from the accelerometer
+ * Sets up a Window and BitmapLayer, loads all of the PNGs that make up the animation ad GBitmaps, 
+ then advances through the frames when the data from the accelerometer indicates that the user has tilted their watch up
+ to view the image.
+ 
+ If the animation doesn't start, keep turning your wrist until the top of the watch is just past perpendicular to the ground.
  */
 
 #include <pebble.h>
 
-  
+//create the types of variables that we will need in to run the program
+
+//create window object  
 static Window *window;
+//create blank bitmap layer object, that we will later attach to the window
+static BitmapLayer *image_layer;
+
+//create blank bitmap objects that we will later attach to the image layer
 static GBitmap *seasick_the_cat_bmp_01;
 static GBitmap *seasick_the_cat_bmp_02;
 static GBitmap *seasick_the_cat_bmp_03;
@@ -18,26 +28,37 @@ static GBitmap *seasick_the_cat_bmp_08;
 static GBitmap *seasick_the_cat_bmp_09;
 static GBitmap *seasick_the_cat_bmp_10;
 static GBitmap *seasick_the_cat_bmp_11;
-static BitmapLayer *image_layer;
+
+//animation frame counter
 int animation_frame = 1;
+//number of samples to capture per update when grabbing accelerometer data
 int num_samples = 3;
 
 
-//handler
+//accelerometer handler, this code will loop constantly when the program launches at the rate specified by accel_service_set_sampling_rate()
+//function during the initialization phase.
 static void data_handler(AccelData *data, uint32_t num_samples) {
 
   
   double z_val = data[0].z;
   
+  //if the watch is tipped past ~90 deg advance the animation frame by one 
   if(z_val > 0 && animation_frame < 11){
     ++animation_frame;
   }
+  //if the watch is not being tipped past ~90 deg, move one frame back
   else if(z_val < 0 && animation_frame > 1)
   {
     --animation_frame;
   }
 
-  
+  //big ol' switch statement to animate this mess.
+  //note that if the animation reaches the last frame,
+  //the animation_frame counter is set to 5 before breaking out of the code block
+  //this is so that the animation will continue to run, but will start over about half-way
+  //through the loop to avoid the animation running backwards from an unnatural point in the series
+  //(e.g. the barf flying back into the cat's mouth, yeesh)
+  //it will also allow the animation to keep running from that point so as to crack your friends up.
     switch (animation_frame){
       case 1:
       bitmap_layer_set_bitmap(image_layer, seasick_the_cat_bmp_01);
@@ -83,8 +104,17 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
+  //get the boundary of the root window layer
   GRect bounds = layer_get_bounds(window_layer);
-  //load up frames of animation
+  //create the bitmap layer and make it the same size as the window so that it takes up the whole screen
+  image_layer = bitmap_layer_create(bounds);
+  
+  
+  bitmap_layer_set_background_color(image_layer, GColorClear);
+  
+  //load up frames of animation into the bitmap objects we established at the top of the code
+  //TODO: remember to come back and swap these PNGs out for non-transparent resources, so to 
+  //simplify the resource id needed to instantiate them.
   seasick_the_cat_bmp_01 = gbitmap_create_with_resource(RESOURCE_ID_BARF_01_BLACK);
   seasick_the_cat_bmp_02 = gbitmap_create_with_resource(RESOURCE_ID_BARF_02_BLACK);
   seasick_the_cat_bmp_03 = gbitmap_create_with_resource(RESOURCE_ID_BARF_03_BLACK);
@@ -97,9 +127,7 @@ static void main_window_load(Window *window) {
   seasick_the_cat_bmp_10 = gbitmap_create_with_resource(RESOURCE_ID_BARF_10_BLACK);
   seasick_the_cat_bmp_11 = gbitmap_create_with_resource(RESOURCE_ID_BARF_11_BLACK);
   
-  
-  image_layer = bitmap_layer_create(bounds);
-  bitmap_layer_set_background_color(image_layer, GColorClear);
+  //set the initial frame that we want to appear on the screen when the app loads up
   bitmap_layer_set_bitmap(image_layer, seasick_the_cat_bmp_01);
 
   layer_add_child(window_layer, bitmap_layer_get_layer(image_layer));
